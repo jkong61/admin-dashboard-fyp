d40d8ec8e0b11815525c858861d2ef236058e88d
jkong61 <jonathan.kong.61@outlook.com>
2021-09-09 10:46:54 +0800

    Transfer from Parent Repository

---
+++ src/router/MainRoute.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import CoreContainer from 'containers/CoreContainer'
+import AuthContainer from 'containers/AuthContainer'
+import { AuthProvider } from 'providers/AuthProvider'
+import { ThemeProvider } from 'providers/ThemeProvider'
+import { AssignmentProvider } from 'providers/AssignmentProvider'
+
+export default function MainRoute () {
+  return (
+    <AuthProvider>
+      <ThemeProvider>
+        <AssignmentProvider>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={AuthContainer} />
+              <Route
+                exact
+                path={['/home', '/dashboard', '/charts', '/patients', '/patients/:id']}
+                component={CoreContainer}
+              />
+              <Route
+                path="*"
+                render={({ location }) => (
+                  <Redirect to={{ pathname: '/', state: { from: location } }} />
+                )}
+              />
+            </Switch>
+          </Router>
+        </AssignmentProvider>
+      </ThemeProvider>
+    </AuthProvider>
+  )
+}
